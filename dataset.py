Fix normalization in transforms.

dataset.py
@@ -4,14 +4,13 @@ from torch.utils.data import Dataset
 from package import transforms
 
 transform_train = transforms.Compose([
-    # transforms.RandomScale(range(28, 38)),
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.RandomYFlip(),
     transforms.RandomZFlip(),
-    transforms.ZeroOut(4),
+    # transforms.ZeroOut(4),
     transforms.ToTensor(),
-    transforms.Normalize((0.5), (0.5)),  # need to cal mean and std, revise norm func
+    transforms.Normalize(),
 ])
 
 
@@ -32,8 +31,6 @@ class LIDCIDRIDataset(Dataset):
         mask = np.load(f'{self.root_dir}/resampled_mask/{self.sequence[idx]}.npy').astype(np.float32)
         seg = np.load(f'{self.root_dir}/resampled_seg/{self.sequence[idx]}.npy').astype(np.float32)
         
-        # mask = np.load(f'{self.root_dir}/mask/{self.sequence[idx]}.npy')
-        # seg = np.load(f'{self.root_dir}/seg/{self.sequence[idx]}.npy')
         input = self.transform(mask * seg)
 
         row = self.sr[self.sr['seg_id'] == self.sequence[idx]]
@@ -42,21 +39,3 @@ class LIDCIDRIDataset(Dataset):
 
         return input, label
 
-# ids = np.load('dataset/seg_ids.npy')
-# max_hu = 0
-# min_hu = 3000
-# count = 0
-# for id in ids:
-#     count += 1
-#     max_hu = max(np.max(np.load(f'dataset/cropped_resampled_seg/{id}.npy')), max_hu)
-#     min_hu = min(np.min(np.load(f'dataset/cropped_resampled_seg/{id}.npy')), min_hu)
-# print(f'(min_hu, max_hu) = {(min_hu, max_hu)}, count = {count}')
-
-# nomalization of hu value
-# def transform(pixels):
-#     new_pixels = (pixels - min_hu) / (max_hu - min_hu)
-#     new_pixels = new_pixels.astype(np.float32)
-#     # new_pixels = new_pixels.reshape(1, *new_pixels.shape)
-#     # print(f'pixel shape = {new_pixels.shape}')
-    
-#     return new_pixels # new_pixels

package/transforms.py
@@ -88,35 +88,26 @@ class Compose(object):
 
 
 class Normalize(object):
-    """Normalize an tensor image with mean and standard deviation.
+    def __init__(self):
+        ids = np.load('dataset/seg_ids.npy')
+        max_hu = 0
+        min_hu = 3000
+        count = 0
 
-    Given mean: (R, G, B) and std: (R, G, B),
-    will normalize each channel of the torch.*Tensor, i.e.
-    channel = (channel - mean) / std
+        for id in ids:
+            seg = np.load(f'dataset/cropped_resampled_seg/{id}.npy')
+            count += 1
+            max_hu = max(np.max(seg), max_hu)
+            min_hu = min(np.min(seg), min_hu)
+        print(f'(min_hu, max_hu) = {(min_hu, max_hu)}, count = {count}')
 
-    Args:
-        mean (sequence): Sequence of means for R, G, B channels respecitvely.
-        std (sequence): Sequence of standard deviations for R, G, B channels
-            respecitvely.
-    """
+        self.max_hu = max_hu
+        self.min_hu = min_hu
+        self.count = count
 
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, tensor):
-        """
-        Args:
-            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
-
-        Returns:
-            Tensor: Normalized image.
-        """
-        # TODO: make efficient
-        # for t, m, s in zip(tensor, self.mean, self.std):
-
-        tensor.sub_(self.mean).div_(self.std)
-        return tensor
+    def __call__(self, pixels):
+        new_pixels = (pixels - self.min_hu) / (self.max_hu - self.min_hu)
+        return new_pixels
 
 
 from scipy.ndimage.interpolation import zoom